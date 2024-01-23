c5e324d1e3a7f3ef784ecfd0df67ee22a2903c32
Kanykei <momuntaevakanykei@gmail.com>
2024-01-23 11:16:20 +0600

    scrum-162/Feature-Visa Payment

---
+++ src/Payment/VidsaPayment.java
@@ -0,0 +1,7 @@
+package Payment;
+
+public class VidsaPayment {
+    public void payByVisa(String cardHolderName, int cardNumber){
+        System.out.println(cardHolderName + " " +cardNumber);
+    }
+}
